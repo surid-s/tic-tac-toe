a61aec668cb64610382b68fe1f697a04a754783f
surid-s <psurids@gmail.com>
2024-10-07 01:17:42 +0600

    handleClick function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
-function Square() {
+function Square({ value }) {
+
+  function handleClick() {
+    console.log("Clicked");
+  }
+
   return (
-    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">X</button>
+    <button
+      className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
+      onClick={handleClick}>
+      {value}
+    </button>
   )
 }
 
@@ -9,19 +18,19 @@ export default function Board() {
   return (
     <>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
 
     </>
