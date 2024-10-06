104268e2c48f71beed1b937f4c558d959a5f292a
surid-s <psurids@gmail.com>
2024-10-07 01:06:03 +0600

    Square component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-export default function Square() {
+function Square() {
+  return (
+    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">X</button>
+  )
+}
+
+
+export default function Board() {
   return (
     <>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">X</button>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">X</button>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">X</button>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">X</button>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+
     </>
   )
 }
